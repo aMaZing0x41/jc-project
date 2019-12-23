c600a289ab06d6391a2baf16ae1eb4df7e20d251
Mario <marioa@gmail.com>
2019-12-23 12:30:55 -0700

    docs

--- cmd/main.go
+++ cmd/main.go
@@ -11,14 +11,18 @@ import (
 var keys = []string{"test1", "test2", "test3", "test4"}
 
 //TODO: doc this
-const NUM_ITERS = 1000000
+const NUM_ITERS = 100000
 
+// This is a simple test harness for the action package
+// NUM_ITERS results in many go routines. Keep the number
+// reasonable. The harness will wait for a bit every few
+// iterations in order to keep the number of active routines
+// manageable.
 func main() {
 	err := action.AddAction("{}")
 	if err != nil {
 		fmt.Println(fmt.Sprintf("ERROR: %v", err))
 	}
-	go action.AddAction("{}")
 	go action.AddAction("{")
 	go action.AddAction(`{"happy":true}`)
 
