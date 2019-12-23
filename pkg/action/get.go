8ca90eabb036261afc1adab2b65a2cfb5ccbeaa7
Mario <marioa@gmail.com>
2019-12-23 16:15:45 -0700

    get tests WIP

--- pkg/action/get.go
+++ pkg/action/get.go
@@ -6,6 +6,12 @@ package action
 
 import "encoding/json"
 
+// Internal type that represents an action with avg time
+type actionAvg struct {
+	Action string  `json:"action,omitempty"`
+	Avg    float32 `json:"avg,omitempty"`
+}
+
 //TODO: write tests
 func GetStats() string {
 	go getStatsInternal()
@@ -14,9 +20,22 @@ func GetStats() string {
 
 func getStatsInternal() {
 	//TODO: needs to return different structure
+	actionsCopy := copyActions()
+	stats := []actionAvg{}
+	for k, v := range actionsCopy {
+		stats = append(stats, actionAvg{Action: k, Avg: v})
+	}
 
-	lock.RLock()
-	result, _ := json.Marshal(actions)
-	lock.RUnlock()
+	result, _ := json.Marshal(stats)
 	statsInfoChan <- string(result)
+}
+
+func copyActions() map[string]float32 {
+	lock.RLock()
+	defer lock.RUnlock()
+	copy := make(map[string]float32, len(actions))
+	for k, v := range actions {
+		copy[k] = v
+	}
+	return copy
 }
